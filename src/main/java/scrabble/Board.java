c80fb372e815913ab01d0a6aa5b779d19a2d06d9
EannaCurran <emj.curran@gmail.com>
2020-02-14 10:32:27 +0000

    Created methods for Board

--- src/main/java/scrabble/Board.java
+++ src/main/java/scrabble/Board.java
@@ -1,4 +1,31 @@
 package scrabble;
 
+import java.util.ArrayList;
+
 public class Board {
+
+    // Johns
+    public Board(){
+
+    }
+
+    // Killian
+
+    public void resetBoard(){
+
+    }
+
+    public void printBoard(){
+
+    }
+
+    // Eanna
+
+    public boolean checkValidMove(ArrayList<Tile> word){
+        return true;
+    }
+
+    public void placeWord(ArrayList<Tile> word){
+
+    }
 }
