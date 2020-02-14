42c357408fca4c003c8768a491b4e826af418ee2
John <odonnell88j@gmail.com>
2020-02-14 08:44:27 +0000

    Added SquareType enum to Square Class

--- src/main/java/scrabble/Square.java
+++ src/main/java/scrabble/Square.java
@@ -1,4 +1,14 @@
 package scrabble;
 
 public class Square {
+
+    enum SquareType{
+        NORMAL,
+        START,
+        DOUBLE_WORD,
+        TRIPLE_WORD,
+        DOUBLE_LETTER,
+        TRIPLE_LETTER;
+    }
+
 }
