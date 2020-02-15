c275d8781f139d9d3c8f842b81a034cf0f7909d0
Killian-Callaghan <killiancallaghan99@gmail.com>
2020-02-15 14:55:14 +0000

    Made toString method in the Board class to print the board. Requires a 2D array to be added before it will print anything in the square grid.

--- src/main/java/scrabble/Board.java
+++ src/main/java/scrabble/Board.java
@@ -15,21 +15,53 @@ public class Board {
 
     }
 
+
+    /**
+     * toString method that prints the board
+     * @return Returns the board as a string
+     */
     @Override
     public String toString(){
-        String result = "/t/t Scrabble Board";
+                        //The board's title
+        String result = "\t\t\t\t\t\t\t\tScrabble Board\n  |";
 
-        for(int i=0;i<15;i++)
+
+        //for loop creates the number columns
+        for(int n=0; n<15; n++)
         {
-            result = result + "\n________________________________________________\n";
-
-            if(i<9)
+            if(n<9)
             {
-                result = result + " ";
+                result = result + " " + (n+1) + "  |";
+            }
+            else
+            {
+                result = result + " " + (n+1) + " |";
             }
         }
 
-        return null;
+        //For loop for the row index
+        for(int i=0;i<15;i++)
+        {
+            result = result + "\n______________________________________________________________________________\n";
+
+            //If statement checks if row number is not a double digit
+            if(i<9)
+            {
+                //Gives single digit numbers the same spacing as double digits
+                result = result + " ";
+            }
+            result = result + (i+1) + "|";
+
+            //Loop for the column index
+            for(int j=0; j<15; j++)
+            {
+                //PLACEHOLDER
+                result = result + "    |";
+                //result = result + boardSquares[i][j] + "|";
+            }
+        }
+
+        return result;
     }
 
     // Eanna
