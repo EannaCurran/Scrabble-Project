d777a3e30ce497dfe606175f40ed1fbf1333699f
John <odonnell88j@gmail.com>
2020-03-10 16:43:32 +0000

    createPlayer method in Scrabble Class

--- src/main/java/scrabble/Scrabble.java
+++ src/main/java/scrabble/Scrabble.java
@@ -1,5 +1,7 @@
 package scrabble;
 
+import scrabble.exceptions.InvalidScrabbleException;
+
 public class Scrabble {
 
     /**
@@ -13,17 +15,47 @@ public class Scrabble {
     private Pool pool;
 
     /**
-     * Player one of the Scrabble Game
+     * Player one and two of the Scrabble Game
      */
-    private Player player1;
+    private Player[] players;
 
     /**
-     * Player one of the Scrabble Game
+     * Player turn order
+     *
+     * False player one goes first
+     * True Player two goes First
      */
-    private Player player2;
+    private Boolean playerOrder;
 
+    /**
+     * Scrabble Game Constructor
+     *
+     * Creates a new game of Scrabble
+     */
     public Scrabble(){
+        board = new Board();
 
+        pool = new Pool();
+
+        players = new Player[2];
+
+        playerOrder = false;
+    }
+
+    /**
+     * Method to create a player with an inputted name
+     *
+     * @param name The name of the player
+     * @param playerNumber The index of the player in the players array
+     * @throws InvalidScrabbleException If the index is out of bounds of the players array
+     */
+    public void createPlayer(String name, int playerNumber){
+        if (playerNumber <= 0 || playerNumber < players.length){
+            players[playerNumber] = new Player(name, pool);
+        }
+        else{
+            throw new InvalidScrabbleException("Please select either player 1 or 2\n");
+        }
     }
 
 }
