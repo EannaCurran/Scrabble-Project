abad5da6f7b906da5fd0676804f10d31071edc8b
John <odonnell88j@gmail.com>
2020-03-10 23:51:27 +0000

    Created Word Class Accessor Methods and Constructor

--- src/main/java/scrabble/Word.java
+++ src/main/java/scrabble/Word.java
@@ -1,4 +1,40 @@
 package scrabble;
 
+
+import scrabble.exceptions.InvalidWordException;
+
 public class Word {
+
+    private int[] startPosition;
+
+    private  char[] word;
+
+    private  UserInput.Direction direction;
+
+    public Word(int[] coOrdinates, UserInput.Direction d, char[] w){
+        if (Board.checkValidPosition(coOrdinates) && validWord(w)){
+            startPosition = coOrdinates;
+            direction = d;
+            word = w;
+        }else {
+            throw new InvalidWordException("The word co-ordinates must be on the Board\nThe word must between 1 - 15 capital letters long\n");
+        }
+    }
+
+    public char[] getWord() {
+        return word;
+    }
+
+    public UserInput.Direction getDirection() {
+        return direction;
+    }
+
+    public int[] getStartPosition() {
+        return startPosition;
+    }
+
+    private boolean validWord(char[] w){
+        //A word must have between 1 - 15 letters and contain only capital letters
+        return new String(w).matches("^[A-Z]{1,15}+");
+    }
 }
