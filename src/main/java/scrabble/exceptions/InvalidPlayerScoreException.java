9dc05ad8c5b8edd42ce71ebe1b63b88b01560732
John <odonnell88j@gmail.com>
2020-03-10 16:32:47 +0000

    Score Exception Commenting

--- src/main/java/scrabble/exceptions/InvalidPlayerScoreException.java
+++ src/main/java/scrabble/exceptions/InvalidPlayerScoreException.java
@@ -1,6 +1,8 @@
 package scrabble.exceptions;
 
-//Custom Exception for Score in Player Class
+/**
+ * Custom Exception for Score in Player Class
+ */
 public class InvalidPlayerScoreException extends IllegalArgumentException{
     public  InvalidPlayerScoreException(String s){
         super(s);
