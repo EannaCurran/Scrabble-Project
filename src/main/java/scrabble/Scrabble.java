3a4407c73f32044a2b43e3b877599057ac757b21
John <odonnell88j@gmail.com>
2020-03-10 15:59:01 +0000

    Scrabble Class instance Variables

--- src/main/java/scrabble/Scrabble.java
+++ src/main/java/scrabble/Scrabble.java
@@ -1,4 +1,29 @@
 package scrabble;
 
 public class Scrabble {
+
+    /**
+     * The Scrabble Board
+     */
+    private Board board;
+
+    /**
+     * The Pool of Tiles for the Scrabble Game
+     */
+    private Pool pool;
+
+    /**
+     * Player one of the Scrabble Game
+     */
+    private Player player1;
+
+    /**
+     * Player one of the Scrabble Game
+     */
+    private Player player2;
+
+    public Scrabble(){
+
+    }
+
 }
