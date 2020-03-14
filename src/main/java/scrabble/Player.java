456bfbb07430d5c459d54f3e275ac36f2165f1d4
John <odonnell88j@gmail.com>
2020-03-14 16:37:32 +0000

    Commenting Player and Word

--- src/main/java/scrabble/Player.java
+++ src/main/java/scrabble/Player.java
@@ -44,7 +44,7 @@ public class Player {
             this.playerFrame = new Frame(poolRef);
         }
         else{
-            throw new InvalidPlayerNameException("Invalid name for Player.\nPlayer's name must be less than of equal to 24 characters\nPlayer's can only contain letters and digits\nPlayer's name can not be null\nInputted name: " + namePlayer);
+            throw new InvalidPlayerNameException("Invalid name for Player.\nPlayer's name must be less than of equal to 24 characters\nPlayer's can only contain letters and digits\nPlayer's name can not be null\nInputted name: " + namePlayer + "\n");
         }
     }
 
@@ -89,7 +89,7 @@ public class Player {
             this.name = newName;
         }
         else{
-            throw new InvalidPlayerNameException("Invalid name for Player.\nPlayer's name must be less than of equal to 24 characters\nPlayer's can only contain letters and digits\nPlayer's name can not be null\nInputted name: " + newName);
+            throw new InvalidPlayerNameException("Invalid name for Player.\nPlayer's name must be less than of equal to 24 characters\nPlayer's can only contain letters and digits\nPlayer's name can not be null\nInputted name: " + newName + "\n");
         }
     }
 
@@ -117,7 +117,7 @@ public class Player {
             this.name = name;
         }
         else{
-            throw new InvalidPlayerNameException("Invalid name for Player.\nPlayer's name must be less than of equal to 24 characters\nPlayer's can only contain letters and digits\nPlayer's name can not be null\nInputted name: " + name);
+            throw new InvalidPlayerNameException("Invalid name for Player.\nPlayer's name must be less than of equal to 24 characters\nPlayer's can only contain letters and digits\nPlayer's name can not be null\nInputted name: " + name + "\n");
         }
     }
 
@@ -144,7 +144,7 @@ public class Player {
             this.score += scoreIncrease;
         }
         else {
-            throw new InvalidPlayerScoreException("Players Score can not be increased by a negative value");
+            throw new InvalidPlayerScoreException("Players Score can not be increased by a negative value\n");
         }
     }
 
@@ -161,7 +161,7 @@ public class Player {
             this.score += scoreDecrease;
         }
         else {
-            throw new InvalidPlayerScoreException("Players Score can not be decreased by a negative value");
+            throw new InvalidPlayerScoreException("Players Score can not be decreased by a negative value\n");
         }
     }
 
@@ -176,7 +176,7 @@ public class Player {
         return this.playerFrame;
     }
 
-    //private Board playerBoard;
+
 
 
 
