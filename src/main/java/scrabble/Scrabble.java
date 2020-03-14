64d84f2c275eb1c249ab8396b160c09326778796
John <odonnell88j@gmail.com>
2020-03-14 20:09:17 +0000

    Scrabble playerMove and challenge WIP

--- src/main/java/scrabble/Scrabble.java
+++ src/main/java/scrabble/Scrabble.java
@@ -2,6 +2,7 @@ package scrabble;
 
 import scrabble.exceptions.InvalidScrabbleException;
 
+import javax.swing.event.ChangeEvent;
 import java.util.ArrayList;
 
 public class Scrabble {
@@ -63,22 +64,57 @@ public class Scrabble {
             players[playerNumber] = new Player(name, pool);
         }
         else{
-            throw new InvalidScrabbleException("Please select either player 1 or 2\n");
+            throw new InvalidScrabbleException("Please select either player 1 or 2.\n");
         }
     }
 
+    /**
+     * Method to complete a Player move
+     *
+     * @param startPosition Start position of the Word
+     * @param direction Direction of the Word
+     * @param word Word in char array form
+     * @param player Player making the move
+     */
     public void playerMove(int[] startPosition, UserInput.Direction direction, char[] word, Player player){
         if (startPosition.length == 2 && board.checkValidPosition(startPosition)){
 
-            moveHistory.add(0, new MoveInfo(player,startPosition, direction, word));
+            MoveInfo move = new MoveInfo(player,startPosition, direction, word);
 
-            if (board.checkValidMove(moveHistory.get(0))){
+            board.placeTiles(move);
 
-            }
+
+                if(challenge()){
+
+                    board.removeMove(move);
+
+                    player.decreaseScore(moveHistory.get(0).getMoveScore());
+
+                }
+                else{
+
+                    board.setWordSquaresNormal(moveHistory.get(0).getPrimaryWord());
+
+                    player.getPlayerFrame().fillFrame();
+
+                    moveHistory.add(move);
+
+                    //TODO Skip other player turn
+                }
+
         }
         else {
-            throw new InvalidScrabbleException("Invalid Start Position Inputted\n");
+            throw new InvalidScrabbleException("Invalid Start Position Inputted.\n");
         }
     }
+
+    public boolean challenge(){
+        boolean challenge = false;
+
+
+        return challenge;
+    }
+
+
 
 }
