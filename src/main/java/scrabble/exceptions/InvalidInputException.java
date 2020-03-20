bd2d571a54955a60aff12366a2a33a45fe370733
Killian-Callaghan <killiancallaghan99@gmail.com>
2020-03-20 18:37:02 +0000

    Commenting for InvalidInputException.

--- src/main/java/scrabble/exceptions/InvalidInputException.java
+++ src/main/java/scrabble/exceptions/InvalidInputException.java
@@ -1,5 +1,8 @@
 package scrabble.exceptions;
 
+/**
+ * Custom exception for UserInput Class
+ */
 public class InvalidInputException extends IllegalArgumentException{
     public  InvalidInputException(String s){
         super(s);
